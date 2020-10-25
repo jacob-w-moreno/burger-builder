5f08faf5039fe8b3e67fdbbc7735f03eea09e404
Jake <jacob.w.moreno@gmail.com>
2020-10-25 16:35:00 -0600

    The burger is officially live.

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
-import classes from './Layout.css';
+import styles from './Layout.module.css';
 
-console.log('classes:', classes);
+console.log('styles:', styles);
 
 const layout = props => <Aux>
   <div>Toolbar, SideDrawer, Backdrop</div>
-  <main className={classes.Content}>
+  <main className={styles.Content}>
     {props.children}
   </main>
 </Aux>
