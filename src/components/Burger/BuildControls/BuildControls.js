6a2a3eae90368a4fe5f87c1727050e798a6afcdd
Jake <jacob.w.moreno@gmail.com>
2020-10-25 22:14:49 -0600

    The burger builder is nearly up and running! Users can now add and delete ingredients dynamically.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl';
+import styles from './BuildControls.module.css';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+]
+
+const BuildControls = props => {
+  return (<div className={styles.BuildControls}>
+    {controls.map(control =>
+      <BuildControl
+        key={control.label}
+        label={control.label}
+        added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
+      />
+    )}
+  </div>);
+};
+
+export default BuildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,25 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = props => {
+
+// This creates an array of the keys (str) of the ingredients object.
+  let transformedIngredients = Object.keys(props.ingredients)
+// For every ingredient, an array is created with as many undefined values as the value of the key in the object.
+    .map(igKey => [...Array(props.ingredients[igKey])]
+// For every undefined value, an array is created with a BurgerIngredient inside. The type is passed in the form of the key that was extracted from the original object.
+    .map((_, i) => <BurgerIngredient key={igKey + i} type={igKey}/>))
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please Start Adding Ingredients!</p>
+    }
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type='bread-top'/>
-      <BurgerIngredient type='cheese'/>
-      <BurgerIngredient type='meat'/>
-      <BurgerIngredient type='bacon'/>
-      <BurgerIngredient type='salad'/>
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom'/>
     </div>
   )
