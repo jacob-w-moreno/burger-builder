755b027cc1ab658870d7604fad5a84e32ebedcc6
Jake <jacob.w.moreno@gmail.com>
2020-10-28 10:48:45 -0600

    Navigation is working

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import styles from './NavigationItem.module.css';
+
 const NavigationItem = props => (
-  <li><a href="/">A link</a></li>
+  <li className={styles.NavigationItem}>
+    <a
+    href={props.link}
+    className={props.active ? styles.active : null}>
+      {props.children}
+    </a>
+  </li>
 )
 
 export default NavigationItem;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = props => (
   <header className={styles.Toolbar}>
     <div>MENU</div>
     {/* <div>LOGO</div> */}
     <Logo/>
-    <nav>
-      ...
-    </nav>
+    <NavigationItems/>
   </header>
 )
 
