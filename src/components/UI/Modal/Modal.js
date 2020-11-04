4ca545c8eca00833e91d540a9db60c5d1a12fe73
Jake <jacob.w.moreno@gmail.com>
2020-11-03 18:13:15 -0700

    Changed the project structure... The app is complete! It is done. Let's go.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
 class Modal extends Component {
-  
+
   shouldComponentUpdate(nextProps, nextState){
     return nextProps.show !== this.props.show;
   }
