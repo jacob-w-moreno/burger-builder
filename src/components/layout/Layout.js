0f645af01c0d69b67ae7786b404b32d90de98b75
Jake <jacob.w.moreno@gmail.com>
2020-11-03 17:43:58 -0700

    SideDrawer toggles open and closed now.

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -9,16 +9,23 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerClosedHandler = () => {
     this.setState({showSideDrawer: false});
   }
 
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
   render () {
+    console.log(this.state.showSideDrawer);
     return(<Aux>
-      <Toolbar/>
+      <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
       <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
       <main className={styles.Content}>
         {this.props.children}
